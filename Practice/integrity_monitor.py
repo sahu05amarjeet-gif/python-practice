bf163df6efcec26521ae64c312a94e36230db54d
sahu05amarjeet-gif <sahu05amarjeet@gmail.com>
2026-09-13 11:55:03 +0530

    Updated the code visibility by adding the new functions to avoid DRY, made a function to handle the comparison between the old and new hashes, updated the new hashes and dumped it on json file when the file is absent, ABSENT file will be automatically created

--- Practice/integrity_monitor.py
+++ Practice/integrity_monitor.py
@@ -29,52 +29,63 @@ def printSummary():
 
 userEnterFolderPath = input("Enter the folder path: ")
 new_hashes = {}
+
+def folderScan(userEnterFolderPath):
+    new_hashes = {}
+    contentsOfFolder = Path(userEnterFolderPath)
+    for item in contentsOfFolder.iterdir():
+        if item.is_file():
+            with open(item, "rb") as file:
+                digest = hashlib.file_digest(file, "sha256")
+                finalHashResult = digest.hexdigest()
+                new_hashes[item.name] = finalHashResult
+    return new_hashes
+
+def comparison(old_hashes, new_hashes): #requires parameters because the func doesn't know what we are iterating over
+    for key in new_hashes:
+        if key in old_hashes:
+            if new_hashes[key] != old_hashes[key]:
+                print(f"'{key}' was modified")
+                global countModifiedFiles #to modify the variable inside the function, python will not think it as a new variable
+                countModifiedFiles+=1
+            else:
+                print("File unchanged")
+                global countUnchangedFiles
+                countUnchangedFiles+=1
+        if key not in old_hashes:
+            print(f"New file detected: '{key}'")
+            global countNewFilesDetected
+            countNewFilesDetected+=1
+                
+    #Checking if the file is not in the new scanned result, if not then printing it's name
+    for key in old_hashes:
+        if key not in new_hashes:
+            missingFile = key
+            print(f"File deleted: {missingFile}")
+            global countDeletedFiles
+            countDeletedFiles+=1
+
 try:
     if os.path.exists(filePath):
         old_hashes = load_json() 
-        contentsOfFolder = Path(userEnterFolderPath)
-        for item in contentsOfFolder.iterdir():
-            if item.is_file():
-                with open(item, "rb") as file:
-                    digest = hashlib.file_digest(file, "sha256")
-                    finalHashResult = digest.hexdigest()
-                    new_hashes[item.name] = finalHashResult
-
-                #Check if the file exists in the old_hashes json file and then compare it.
-                if item.name in old_hashes: 
-                    if (new_hashes[item.name] != old_hashes[item.name]):
-                        print(f"'{item.name}' was modified")
-                        countModifiedFiles+=1
-                    else:
-                        print("File unchanged")
-                        countUnchanedFiles+=1
-                if item.name not in old_hashes:
-                    print(f"New file detected: '{item.name}'")
-                    countNewFilesDetected+=1
-
-        #Checking if the file is not in the new scanned result, if not then printing it's name
-        for key in old_hashes:
-            if key not in new_hashes:
-                missingFile = key
-                print(f"File deleted: {missingFile}")
-                countDeletedFiles+=1
+        new_hashes = folderScan(userEnterFolderPath)
+        comparison(old_hashes, new_hashes)
+        printSummary()
     else:
-        new_hashes = {}
-        scanTheFolder = Path(userEnterFolderPath)
-        for folder in scanTheFolder.iterdir():
-            if folder.is_file:
-                with open(folder, "rb") as file:
-                    hashDigest = hashlib.file_digest("sha256")
-                    finalHash = hashDigest.hexdigest()
-                    new_hashes[folder.name] = finalHash
+        new_hashes = folderScan(userEnterFolderPath)
+        with open(filePath, "w") as file:
+            json.dump(new_hashes, file)
+        print("File has been created\nPlease run the program again")
 
-    printSummary()
     update_json()
 
 except FileNotFoundError:   
     print("Folder not found")
+
+except json.JSONDecodeError:
+    print("Json file is empty")
+
 except NotADirectoryError:
     print("Not a folder")
 
-
-
+#Note: This program doesn't read the sub-folders inside a directory, it'll be a feature for the upcoming commits.
